74fa08084bc277ef6c4fb740436dfe4ade531644
Vitória Vasconcelos <vitoria.mvsv@gmail.com>
2025-11-12 10:24:47 -0300

    correcao nos links das imagens

--- src/css/javascript/scripts.js
+++ src/css/javascript/scripts.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const artiststData = [
-        {name:'System Of a Down', image: '/src/imagens/systemofadown.jpg'},
-        {name: 'Linkin Park', image:'/src/imagens/Linkin_Park.jpeg'},
-        {name:'Avenged Sevenfold',image:'/src/imagens/a7x.jpg'},
-        {name:'Metallica', image:'/src/imagens/metallica-1.jpg'},
-        {name: 'Slipknot', image:'/src/imagens/slipknot.jpg'},
-        {name:'Evanescence',image: '/src/imagens/EVANESCENCE.jpeg'},
-        {name:'My Chemical Romance ',image: '/src/imagens/my-chemical-romance.jpg'}
+        {name:'System Of a Down', image: './src/imagens/systemofadown.jpg'},
+        {name: 'Linkin Park', image:'./src/imagens/Linkin_Park.jpeg'},
+        {name:'Avenged Sevenfold',image:'./src/imagens/a7x.jpg'},
+        {name:'Metallica', image:'./src/imagens/metallica-1.jpg'},
+        {name: 'Slipknot', image:'./src/imagens/slipknot.jpg'},
+        {name:'Evanescence',image: './src/imagens/EVANESCENCE.jpeg'},
+        {name:'My Chemical Romance ',image: './src/imagens/my-chemical-romance.jpg'}
         
 
     ];
     const albunsData = [
-        {name:'Meteora', artista:'Linkin Park', image: '/src/imagens/meteora.jpg'},
-        {name:'Fallen',artista:'Evanescence',image:'/src/imagens/fallen.jpg'},
-        {name:'Americana',artista:'The Offspring ',image:'/src/imagens/cd-the-offspring-americana.webp'},
-        {name:'Toxicity',artista:'System Of a Down ',image:'/src/imagens/toxicity.jpg'},
-        {name:'Nightmare',artista:'Avenged Sevenfold',image:'/src/imagens/nightmare.jpg'},
+        {name:'Meteora', artista:'Linkin Park', image: './src/imagens/meteora.jpg'},
+        {name:'Fallen',artista:'Evanescence',image:'./src/imagens/fallen.jpg'},
+        {name:'Americana',artista:'The Offspring ',image:'./src/imagens/cd-the-offspring-americana.webp'},
+        {name:'Toxicity',artista:'System Of a Down ',image:'./src/imagens/toxicity.jpg'},
+        {name:'Nightmare',artista:'Avenged Sevenfold',image:'./src/imagens/nightmare.jpg'},
     ];
     
     const artiasPopulares = document.querySelector('.artistas-populares')
